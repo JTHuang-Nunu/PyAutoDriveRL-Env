a0d1e3024d7ffdad5c93a4042697930a17631266
JTH <ge6435wr@gmail.com>
2024-11-16 14:52:04 +0800

    [FIX] 修復Multiprocess and threading無法正確關閉問題

--- CarDataWindow.py
+++ CarDataWindow.py
@@ -2,6 +2,8 @@ import threading
 import tkinter as tk
 import time
 
+import tkinter as tk
+
 class CarDataWindow:
     def __init__(self, shared_dict):
         self.shared_dict = shared_dict
@@ -9,15 +11,33 @@ class CarDataWindow:
         self.window.title("Car Data Monitor")
         self.data_label = tk.Label(self.window, text="Initializing...", font=("Courier", 10), justify=tk.LEFT)
         self.data_label.pack()
+
+        # bind the window close event
+        self.window.protocol("WM_DELETE_WINDOW", self.on_close)
+
         self.update_data()
 
     def update_data(self):
-        data = self.shared_dict.get('car_data')
+        data = self.shared_dict.get('car_data', "No data available")
         self.data_label.config(text=data)
+        # 每 100 毫秒更新一次数据
         self.window.after(100, self.update_data)
+
+    def on_close(self):
+        '''
+        Close the window.
+        '''
+        self.window.destroy()
 
     def start(self):
         self.window.mainloop()
+
+    def stop(self):
+        """
+        Stop the GUI thread.
+        """
+        self.on_close()
+
 
 class CarDataWindowBAK:
     def __init__(self, share_dict):
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import signal
+import threading
 from train.train_stable_baseline_my import *
 from multiprocessing import Process, Manager
 from CarDataWindow import CarDataWindow
@@ -7,16 +8,20 @@ from utils.logger import logger
 # train_car_rl(strategy='PPO', model_mode='new', timesteps=100000, save_timesteps=10)
 # train_car_rl(strategy='PPO', model_mode='load_best', timesteps=100000, save_timesteps=10, n_steps=1000)
 
-# def signal_handler(sig, frame):
-#     print("Shutdown signal received. Terminating all processes...")
-#     train_process.terminate()
-#     # sys.exit(0)
+def signal_handler():
+    print("Shutdown signal received. Terminating all processes...")
+    if train_process.is_alive():
+        train_process.terminate()
+    if car_data_window_thread.is_alive():
+        car_data_window.stop()
+    sys.exit(0)
 
-# signal.signal(signal.SIGINT, signal_handler)
-# signal.signal(signal.SIGTERM, signal_handler)
 
 unit = 64
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    
     with Manager() as manager:
         share_dict = manager.dict()
         # share_dict['car_data'] = str("")
@@ -38,3 +43,39 @@ if __name__ == '__main__':
         car_data_window.start()
 
         train_process.join()
+        car_data_window.stop()
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    with Manager() as manager:
+        share_dict = manager.dict()
+
+        car_data_window = CarDataWindow(share_dict)
+
+        car_data_window_thread = threading.Thread(target=car_data_window.start, daemon=True)
+        car_data_window_thread.start()
+
+        train_process = Process(
+            target=train_car_rl,
+            kwargs={
+                "strategy": "PPO",
+                "model_mode": "load_best",
+                "timesteps": 1000000,
+                "save_timesteps": unit * 100,
+                "n_steps": unit * 5,
+                "batch_size": unit,
+                "share_dict": share_dict
+            }
+        )
+        train_process.start()
+
+        try:
+            train_process.join()
+        except KeyboardInterrupt:
+            logger.info("KeyboardInterrupt detected. Stopping processes...")
+            signal_handler()
+
+        if car_data_window_thread.is_alive():
+            car_data_window.stop()
